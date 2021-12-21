f5c85471ec40cb976e6ceef43715f329ff8359db
Matt DeBone <mdebone@gmail.com>
2021-12-21 00:36:13 -0600

    ohh well

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Import Dependencies
 from flask import Flask, render_template, redirect 
 from flask_pymongo import PyMongo
-import scrape_mars
 
 # create instance of Flask app
 app = Flask(__name__)
